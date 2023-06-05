1467320d720ec3e8e5297d4cb92fe307a0c29fae
alfianbr16 <alfianbr84@gmail.com>
2023-06-05 22:14:45 +0700

    program

---
+++ footer.php
@@ -0,0 +1,24 @@
+
+	<footer style="border-top: 4px solid #ff8680;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #ff8680"><b>MAKANAN HEWAN PELIHARAAN</b></h3>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+	</footer>
+
+</body>
+</html>
